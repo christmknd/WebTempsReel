ffe46e45951935649044820fabd06866e6ba4942
christmknd <christmokonda@gmail.com>
2022-12-06 17:34:16 +0100

    RESET front

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Test from "./components/Test";
-import Home from './views/Home';
-
-function App() {
   return (
     <div className="App">
-      <Home/>
-      <Test/>
     </div>
   );
 }
