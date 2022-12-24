ff125fec23777a7cd97f995e8f43d872d5c93b2c
Maxime Dev <maximedaude.dev@gmail.com>
2022-12-24 23:40:19 +0100

    fix pageNotFound

--- front/src/views/NotFound.js
+++
@@ -1,8 +0,0 @@
-function PageNotfound(){
-    return (
-        <div className="pageNotFound">
-            <Nav />
-            <h1>La page que vous cherchez n'existe pas</h1>
-        </div>
-    )
-}
---
+++ front/src/views/PageNotFound.js
@@ -0,0 +1,12 @@
+import Nav from "../components/Nav";
+
+function PageNotFound() {
+  return (
+    <div className="pageNotFound">
+      <Nav />
+      <h1>La page que vous cherchez n'existe pas</h1>
+    </div>
+  );
+}
+
+export default PageNotFound;
