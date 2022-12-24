15fea47b2c40b638cdf8d4faf4f52bb497bc7061
christmknd <christmokonda@gmail.com>
2022-12-24 22:50:19 +0100

    redisign navigation

---
+++ front/src/views/NotFound.js
@@ -0,0 +1,8 @@
+function PageNotfound(){
+    return (
+        <div className="pageNotFound">
+            <Nav />
+            <h1>La page que vous cherchez n'existe pas</h1>
+        </div>
+    )
+}
